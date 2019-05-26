81725a774664ed8e1e5ab4714ef03217283698f3
Charliiee <carlin.afonso@gmail.com>
2019-05-26 00:22:24 -0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# configure zsh
+# first install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# then install zplug
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+# configure nvim
+ln -s /home/$USER/.vim /home/$USER/.config/nvim
+ln -s /home/$USER/.vimrc /home/$USER/.config/nvim/init.vim
+
+# delete unecessary dotfiles
+rm ~/.README.md
+rm ~/.Screenshot-1.png
+rm ~/.Screenshot-2.png
+rm ~/.LICENSE
