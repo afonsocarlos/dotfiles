fbadff068e8acfb49f992d6aa60dd2da6e8a9a00
Carlos Afonso <afonso.carlos.braga@gmail.com>
2022-12-26 10:57:04 +0000

    Remove vim setup process, set up neovim only

--- install.sh
+++ install.sh
@@ -14,12 +14,6 @@ tic xterm-256color-italic.terminfo
 # test if it worked!
 echo -e "\e[1mbold\e[0m\n\e[3mitalic\e[0m\n\e[4munderline\e[0m\n\e[9mstrikethrough\e[0m"
 
-# configure nvim
-ln -s /home/$USER/.vim /home/$USER/.config/nvim
-ln -s /home/$USER/.vimrc /home/$USER/.config/nvim/init.vim
-
-mkdir -p ~/.tmux/plugins && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
 # delete unecessary dotfiles
 rm ~/.README.md
 rm ~/.Screenshot-1.png
@@ -27,3 +21,9 @@ rm ~/.Screenshot-2.png
 rm ~/.LICENSE
 rm ~/.install.sh
 rm ~/.xterm-256color-italic.terminfo
+
+# setup tmux plugins
+mkdir -p ~/.tmux/plugins && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+#
+# configure nvim
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
