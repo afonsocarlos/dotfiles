c94871ead2ab793a711832152b356e598c044222
Carlos Afonso <afonso.carlos.braga@gmail.com>
2023-10-08 19:21:06 +0100

    Improve lsp navigation in Telescope

--- config/nvim/after/plugin/keymap/lsp.lua
+++ config/nvim/after/plugin/keymap/lsp.lua
@@ -2,31 +2,15 @@ local neogen = require("neogen")
 local telescope_builtin = require "telescope.builtin"
 local default_opts = require("afonsocarlos.keymap").default_opts
 
-local config = {
-  path_display = function(opts, path)
-    local os_sep = require("telescope.utils").get_separator()
-    local found_first = false
-    for i = #path, 1, -1 do
-      if path:sub(i, i) == os_sep then
-        if found_first then
-          return path:sub(i + 1, -1)
-        end
-        found_first = true
-      end
-    end
-    return path
-  end,
-}
-
 -- LSP config (the mappings used in the default file don't quite work right)
 vim.keymap.set("n", "K", vim.lsp.buf.hover, default_opts)
 vim.keymap.set("n", "<leader>gc", neogen.generate, default_opts)
 vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, default_opts)
 vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, default_opts)
-vim.keymap.set("n", "<leader>gd", function() telescope_builtin.lsp_definitions(config) end, default_opts)
-vim.keymap.set("n", "<leader>gr", function() telescope_builtin.lsp_references(config) end, default_opts)
-vim.keymap.set("n", "<leader>gi", function() telescope_builtin.lsp_implementations(config) end, default_opts)
-vim.keymap.set("n", "<leader>gt", function() telescope_builtin.lsp_type_definitions(config) end, default_opts)
+vim.keymap.set("n", "<leader>gd", ":Telescope lsp_definitions path_display={'tail'}<CR>", default_opts)
+vim.keymap.set("n", "<leader>gr", ":Telescope lsp_references path_display={'tail'}<CR>", default_opts)
+vim.keymap.set("n", "<leader>gi", ":Telescope lsp_implementations path_display={'tail'}<CR>", default_opts)
+vim.keymap.set("n", "<leader>gt", ":Telescope lsp_type_definitions path_display={'tail'}<CR>", default_opts)
 vim.keymap.set("n", "<leader>gh", vim.lsp.buf.signature_help, default_opts)
 vim.keymap.set("n", "<leader>ge", vim.diagnostic.open_float, default_opts)
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, default_opts)
