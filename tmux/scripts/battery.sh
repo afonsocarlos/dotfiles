741c919d67aa79747a73c856b90cd8fe27c7cab3
Carlos Afonso <afonso.carlos.braga@gmail.com>
2023-11-13 12:54:42 +0000

    Improve tmux and kitty for macos

--- tmux/scripts/battery.sh
+++ tmux/scripts/battery.sh
@@ -1,1 +1,5 @@
-echo `upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage/ { print ($2=="0%" ? "100%" : $2) }'`
+if command -v pmset; then
+    echo `pmset -g batt | grep -o "[0-9]\{1,3\}%"`
+else
+    echo `upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage/ { print ($2=="0%" ? "100%" : $2) }'`
+fi
