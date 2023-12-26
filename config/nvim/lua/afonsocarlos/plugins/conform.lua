0e5097a70b35a46d0f2b2ff8358a676373140380
Carlos Afonso <afonso.carlos.braga@gmail.com>
2023-12-26 16:42:20 +0000

    Add formatters for json

--- config/nvim/lua/afonsocarlos/plugins/conform.lua
+++ config/nvim/lua/afonsocarlos/plugins/conform.lua
@@ -9,6 +9,7 @@ return {
         lua = { "stylua" },
         -- Use a sub-list to run only the first available formatter
         javascript = { { "prettierd", "prettier" } },
+        json = { { "fixjson", "prettierd", "prettier" } },
         php = { "pint" },
         -- Conform will run multiple formatters sequentially
         python = { "isort", "black" },
