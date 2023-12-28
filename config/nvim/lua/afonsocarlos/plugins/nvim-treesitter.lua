65f1700faa320bc29488c6f01c559f8c904df909
Carlos Afonso <afonso.carlos.braga@gmail.com>
2023-12-28 16:15:47 +0000

    Update treesitter config. Remove repeatable move

--- config/nvim/lua/afonsocarlos/plugins/nvim-treesitter.lua
+++ config/nvim/lua/afonsocarlos/plugins/nvim-treesitter.lua
@@ -11,12 +11,6 @@ local textobjects = {
       ["if"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
       ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
       ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
-
-      ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
-      ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
-      ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
-      ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
-
     },
     -- You can choose the select mode (default is charwise 'v')
     selection_modes = {
@@ -65,11 +59,10 @@ local textobjects = {
 
 return {
   "nvim-treesitter/nvim-treesitter",
-  dev = true,
   event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
   dependencies = {
-    { "nvim-treesitter/nvim-treesitter-textobjects", lazy = true, dev = true },
+    { "nvim-treesitter/nvim-treesitter-textobjects", lazy = true },
     "windwp/nvim-ts-autotag",
     -- plugin for splitting/joining blocks of code
     {
@@ -96,10 +89,12 @@ return {
         "markdown",
         "markdown_inline",
         "php",
+        "phpdoc",
         "query",
         "sql",
         "typescript",
         "vim",
+        "vimdoc",
       },
       context_commentstring = { enable = true },
       highlight = { enable = true },
@@ -122,22 +117,5 @@ return {
         },
       },
     })
-
-    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-
-    -- Repeat movement with ; and ,
-    -- ensure ; goes forward and , goes backward regardless of the last direction
-    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-
-    -- vim way: ; goes to the direction you were moving.
-    -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-    -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
-
-    -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-    vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-    vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-    vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-    vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
   end
 }
