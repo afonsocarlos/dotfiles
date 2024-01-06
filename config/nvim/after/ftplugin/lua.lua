9375e7aa961e2d0227ef0bff1faec0102dff3592
Carlos Afonso <afonso.carlos.braga@gmail.com>
2024-01-06 19:51:54 +0000

    Add custom splitjoin rules for lua and php

--- config/nvim/after/ftplugin/lua.lua
+++ config/nvim/after/ftplugin/lua.lua
@@ -1,4 +1,23 @@
 vim.opt_local.textwidth = 120
 
 vim.opt_local.shiftwidth = 2
-vim.opt_local.formatoptions:remove { "o" }
+vim.opt_local.formatoptions:remove({ "o" })
+
+MiniSplitjoin = MiniSplitjoin or require("mini.splitjoin")
+
+if MiniSplitjoin == nil then
+  return
+end
+
+local gen_hook = MiniSplitjoin.gen_hook
+local curly = { brackets = { "%b{}" } }
+
+local add_comma_curly = gen_hook.add_trailing_separator(curly)
+local del_comma_curly = gen_hook.del_trailing_separator(curly)
+local pad_curly = gen_hook.pad_brackets(curly)
+
+-- Create buffer-local config
+vim.b.minisplitjoin_config = {
+  split = { hooks_post = { add_comma_curly } },
+  join = { hooks_post = { del_comma_curly, pad_curly } },
+}
--- config/nvim/after/ftplugin/php.lua
+++ config/nvim/after/ftplugin/php.lua
@@ -1,2 +1,21 @@
 vim.opt_local.makeprg = "vendor/bin/phpunit"
 vim.opt.errorformat:append("%E%n)\\ %.%#,%Z%f:%l,%C%m,%C,%-G%.%#")
+-- create vim errorformat for parsing phpunit results
+
+local status, MiniSplitjoin = pcall(require, "mini.splitjoin")
+
+if not status then
+  return
+end
+
+local gen_hook = MiniSplitjoin.gen_hook
+local brackets = { brackets = { "%b[]" } }
+
+local add_comma_brackets = gen_hook.add_trailing_separator(brackets)
+local del_comma_brackets = gen_hook.del_trailing_separator(brackets)
+
+-- Create buffer-local config
+vim.b.minisplitjoin_config = {
+  split = { hooks_post = { add_comma_brackets } },
+  join = { hooks_post = { del_comma_brackets } },
+}
