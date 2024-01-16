7ccef28aa7df026e08a7dd35fb84929e6b90cfb4
Carlos Afonso <afonso.carlos.braga@gmail.com>
2024-01-16 11:05:16 +0000

    Update install script to use stow

--- install.sh
+++ install.sh
@@ -2,25 +2,30 @@
 
 # configure zsh
 # first install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# then install zplug
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# and enable italics to terminal
+tic -x xterm-256color-italic.terminfo
+# test if it worked!
+echo -e "\033[1mbold\033[0m\n\033[3mitalic\033[0m\n\033[4munderline\033[0m\n\033[4:3mUndercurled\033[0m\n\033[9mstrikethrough\033[0m"
+
+# stow dotfiles
+stow cowsay
+stow dbcli
+stow fonts
+stow fortunes
+stow git
+stow keyledsd
+stow kitty
+stow neofetch
+stow nvim
+stow tmux
+stow vim
+stow zsh
 
 # set up fonts
 fc-cache
 
-# and enable italics to terminal
-tic xterm-256color-italic.terminfo
-# test if it worked!
-echo -e "\e[1mbold\e[0m\n\e[3mitalic\e[0m\n\e[4munderline\e[0m\n\e[9mstrikethrough\e[0m"
-
-# delete unecessary dotfiles
-rm ~/.README.md
-rm ~/.Screenshot-1.png
-rm ~/.Screenshot-2.png
-rm ~/.LICENSE
-rm ~/.install.sh
-rm ~/.xterm-256color-italic.terminfo
-
 # setup tmux plugins
-mkdir -p ~/.tmux/plugins && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+TMUX_PLUGINS="${HOME}/.tmux/plugins"
+[ ! -d $TMUX_PLUGINS ] && mkdir -p "${TMUX_PLUGINS}" && git clone https://github.com/tmux-plugins/tpm "${TMUX_PLUGINS}/tpm"
