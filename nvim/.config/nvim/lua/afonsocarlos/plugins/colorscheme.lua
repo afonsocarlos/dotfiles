02e8c360c52a08cab909393677f02a0e60e02772
Carlos Afonso <afonso.carlos.braga@gmail.com>
2024-01-20 10:06:06 +0000

    Add tpope vim-sleuth plugin

--- nvim/.config/nvim/lua/afonsocarlos/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/afonsocarlos/plugins/colorscheme.lua
@@ -1,11 +1,10 @@
 return {
-  -- Colorscheme section
   "navarasu/onedark.nvim", -- OneDark colorscheme for NeoVim
 
   lazy = false,
   priority = 1000,
   config = function()
-    local onedark = require('onedark')
+    local onedark = require("onedark")
     onedark.setup()
     onedark.load()
   end,
