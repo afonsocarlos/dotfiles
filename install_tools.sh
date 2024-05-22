7fc6599c9c330523bc06faf4ec38823e5eb946eb
Carlos Afonso <afonso.carlos.braga@gmail.com>
2024-05-22 23:35:37 +0100

    Skip only apt packages and install other tools

--- install_tools.sh
+++ install_tools.sh
@@ -5,13 +5,6 @@ set -e
 
 mkdir -p ~/build ~/dev ~/work
 
-# check if on debian like distros if not skip the script
-if ! [ -x "$(command -v apt)" ]; then
-  echo "Not on Debian like system"
-  echo "Skipping installing tools..."
-  exit
-fi
-
 # function install {
 #   which $1 &> /dev/null
 #
@@ -23,45 +16,55 @@ fi
 #   fi
 # }
 
-sudo apt update && sudo apt upgrade -y
+# check if on debian like distros if not skip the script
+if [ -x "$(command -v apt)" ]; then
 
-echo 'Installing essential build tools...'
-sudo apt-get install -y \
+  sudo apt update && sudo apt upgrade -y
+
+  echo "Installing essential build tools..."
+  sudo apt-get install -y \
     make cmake git \
     gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip \
     make build-essential libssl-dev zlib1g-dev libbz2-dev \
     libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
     xz-utils tk-dev libffi-dev liblzma-dev git
 
-echo 'Installing tools...'
-sudo apt-get install -y \
-  broot \
-  curl \
-  exfat-utils \
-  flameshot \
-  git \
-  git-trim \
-  htop \
-  httpie \
-  jq \
-  jqp \
-  kitty \
-  neofetch \
-  peek \
-  php \
-  ripgrep \
-  screenkey \
-  tmux \
-  vim \
-  wget \
-  zsh
+  echo "Installing tools..."
+  sudo apt-get install -y \
+    broot \
+    curl \
+    exfat-utils \
+    flameshot \
+    git \
+    git-trim \
+    htop \
+    httpie \
+    jq \
+    jqp \
+    kitty \
+    neofetch \
+    peek \
+    php \
+    ripgrep \
+    screenkey \
+    tmux \
+    vim \
+    wget \
+    zsh
 
-echo 'Installing neovim (from source)...'
+else
+
+  echo "Not on Debian like system"
+  echo "Skipping installing system tools..."
+
+fi
+
+echo "Installing neovim (from source)..."
 if ! [ -d $HOME/build/neovim ]; then
-    git clone https://github.com/neovim/neovim ~/build/neovim
-    cd ~/build/neovim/
-    make CMAKE_BUILD_TYPE=RelWithDebInfo
-    sudo make install
+  git clone https://github.com/neovim/neovim ~/build/neovim
+  cd ~/build/neovim/
+  make CMAKE_BUILD_TYPE=RelWithDebInfo
+  sudo make install
 fi
 
 # PHP tools
