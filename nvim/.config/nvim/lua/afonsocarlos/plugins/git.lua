b26b4c33d954c7a05058b6a420ab245165a2daf4
Carlos Afonso <afonso.carlos.braga@gmail.com>
2024-06-19 14:21:20 +0100

    Update gitsigns config

--- nvim/.config/nvim/lua/afonsocarlos/plugins/git.lua
+++ nvim/.config/nvim/lua/afonsocarlos/plugins/git.lua
@@ -62,6 +62,7 @@ return {
         delay = 500,
       },
       current_line_blame_formatter = "  <author> • <author_time:%R> • <summary>",
+      signs_staged_enable = false,
 
       on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
@@ -72,13 +73,13 @@ return {
         -- Navigation
         vim.keymap.set("n", "]c", function()
           if vim.wo.diff then return "]c" end
-          vim.schedule(function() gs.next_hunk() end)
+          vim.schedule(function() gs.nav_hunk('next') end)
           return "<Ignore>"
         end, { expr = true })
 
         vim.keymap.set("n", "[c", function()
           if vim.wo.diff then return "[c" end
-          vim.schedule(function() gs.prev_hunk() end)
+          vim.schedule(function() gs.nav_hunk('prev') end)
           return "<Ignore>"
         end, { expr = true })
 
