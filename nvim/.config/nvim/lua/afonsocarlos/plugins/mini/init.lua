ade7cb4ffad1ce3606a6197b4a551e2491cb5a48
Carlos Afonso <afonso.carlos.braga@gmail.com>
2024-06-24 12:22:09 +0100

    Redefine some text objects for personal preference

--- nvim/.config/nvim/lua/afonsocarlos/plugins/mini/init.lua
+++ nvim/.config/nvim/lua/afonsocarlos/plugins/mini/init.lua
@@ -7,6 +7,10 @@ return {
       ai.setup({
         custom_textobjects = {
           a = ai.gen_spec.argument({ separator = ",%s*" }),
+          b = { { '%b()' }, '^.().*().$' },
+          B = { { '%b{}' }, '^.%s*().-()%s*.$' },
+          f = false,
+          ["{"] = false,
         },
         n_lines = 500,
         search_method = "cover_or_nearest",
