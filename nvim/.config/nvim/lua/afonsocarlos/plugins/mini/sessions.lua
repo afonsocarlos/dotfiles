0fbb863257d5ca25dacfb466307689eea122fbda
Carlos Afonso <afonso.carlos.braga@gmail.com>
2024-06-28 11:13:12 +0100

    Skip autoloading nvim session if cwd is not in any autosession_directories

--- nvim/.config/nvim/lua/afonsocarlos/plugins/mini/sessions.lua
+++ nvim/.config/nvim/lua/afonsocarlos/plugins/mini/sessions.lua
@@ -1,53 +1,49 @@
 local M = {}
 
 local setup_autosession = function()
-  local has_mini_sessions, MiniSessions = pcall(require, "mini.sessions")
+  local MiniSessions = require("mini.sessions")
+  local lspconfig = require("lspconfig")
 
-      if not has_mini_sessions then
-        return
+  local cwd = vim.fn.getcwd()
+  local project_root = lspconfig.util.find_git_ancestor(cwd)
+
+  if not project_root then
+    MiniSessions.setup({ autoread = false })
+    return
+  end
+
+  local HOME = os.getenv("HOME"):gsub("[.-]", "%%%1")
+  local autosession_directories = {
+    HOME .. "/.dotfiles",
+    HOME .. "/build",
+    HOME .. "/dev",
+    HOME .. "/work",
+  }
+
+  for _, directory in ipairs(autosession_directories) do
+    if cwd:find(directory) then
+      local session_name = project_root:gsub("/", "_")
+
+      local session = MiniSessions.detected[session_name]
+      if not session then
+        vim.cmd("cd " .. project_root)
+        MiniSessions.write(session_name)
+      else
+        -- Hack Minisessions.detected table setting the modify_time for the session for the current directory as the
+        -- greatest so "MiniSessions.get_latest()" retrieves the session for the current directory
+        session.modify_time = vim.fn.getftime(session.path)
+        MiniSessions.detected[session_name] = session
       end
 
-      local lspconfig = require("lspconfig")
+      return
+    end
+  end
 
-      local cwd = vim.fn.getcwd()
-      local project_root = lspconfig.util.find_git_ancestor(cwd)
-
-      if not project_root then
-        MiniSessions.setup({ autoread = false })
-        return
-      end
-
-      local HOME = os.getenv("HOME"):gsub("[.-]", "%%%1")
-      local autosession_directories = {
-        HOME .. "/.dotfiles",
-        HOME .. "/build",
-        HOME .. "/dev",
-        HOME .. "/work",
-      }
-
-      for _, directory in ipairs(autosession_directories) do
-        if cwd:find(directory) then
-          local session_name = project_root:gsub("/", "_")
-
-          local session = MiniSessions.detected[session_name]
-          if not session then
-            vim.cmd("cd " .. project_root)
-            MiniSessions.write(session_name)
-          else
-            -- Hack Minisessions.detected table setting the modify_time for the session for the current directory as the
-            -- greatest so "MiniSessions.get_latest()" retrieves the session for the current directory
-            session.modify_time = vim.fn.getftime(session.path)
-            MiniSessions.detected[session_name] = session
-          end
-        end
-      end
+  -- If cwd is in none of the autosession_directories simply don't autoload session
+  MiniSessions.setup({ autoread = false })
 end
 
 M.setup = function()
-  require("mini.sessions").setup({
-    autoread = true,
-  })
-
   vim.api.nvim_create_autocmd("VimEnter", {
     group = vim.api.nvim_create_augroup("AutoSession", { clear = true }),
     nested = true,
@@ -56,6 +52,9 @@ M.setup = function()
     desc = "Create Session automatically if it doesn't exist",
   })
 
+  require("mini.sessions").setup({
+    autoread = true,
+  })
 end
 
 return M
