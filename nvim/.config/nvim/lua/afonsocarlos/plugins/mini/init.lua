b5a4dce8493c151e5f1d12310ff3bccaed877a49
Carlos Afonso <afonso.carlos.braga@gmail.com>
2024-07-09 18:17:24 +0100

    Update config sub modules require path

--- nvim/.config/nvim/lua/afonsocarlos/plugins/mini/init.lua
+++ nvim/.config/nvim/lua/afonsocarlos/plugins/mini/init.lua
@@ -1,17 +1,15 @@
-local current_path = (...)
-
 return {
   {
     "echasnovski/mini.nvim",
     version = "*",
     config = function()
       -- Custom config
-      require(current_path .. ".ai").setup()
-      require(current_path .. ".bracketed").setup()
-      require(current_path .. ".hipatterns").setup()
-      require(current_path .. ".sessions").setup()
-      require(current_path .. ".splitjoin").setup()
-      require(current_path .. ".surround").setup()
+      require("afonsocarlos.plugins.mini.ai").setup()
+      require("afonsocarlos.plugins.mini.bracketed").setup()
+      require("afonsocarlos.plugins.mini.hipatterns").setup()
+      require("afonsocarlos.plugins.mini.sessions").setup()
+      require("afonsocarlos.plugins.mini.splitjoin").setup()
+      require("afonsocarlos.plugins.mini.surround").setup()
 
       -- Default config
       require("mini.indentscope").setup()
