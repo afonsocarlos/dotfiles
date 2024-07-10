214e517431b064afe47e0e19dbca309125d70235
Carlos Afonso <afonso.carlos.braga@gmail.com>
2024-07-10 13:54:16 +0100

    Simplify auto loading nvim session

--- nvim/.config/nvim/lua/afonsocarlos/plugins/mini/sessions.lua
+++ nvim/.config/nvim/lua/afonsocarlos/plugins/mini/sessions.lua
@@ -1,16 +1,18 @@
 local M = {}
 
+local is_something_shown = function()
+  return vim.fn.argc() > 0 or vim.api.nvim_buf_line_count(0) > 1
+end
+
 local setup_autosession = function()
-  local MiniSessions = require("mini.sessions")
-  local lspconfig = require("lspconfig")
+  local root_git = vim.fs.find({ ".git" }, { upward = true })[1]
 
-  local cwd = vim.fn.getcwd()
-  local project_root = lspconfig.util.find_git_ancestor(cwd)
-
-  if not project_root then
-    MiniSessions.setup({ autoread = false })
+  if not root_git then
     return
   end
+
+  local project_root = vim.fs.dirname(root_git)
+  local session_name = project_root:gsub("/", "%%")
 
   local HOME = os.getenv("HOME"):gsub("[.-]", "%%%1")
   local autosession_directories = {
@@ -21,36 +23,34 @@ local setup_autosession = function()
   }
 
   for _, directory in ipairs(autosession_directories) do
-    if cwd:find(directory) then
-      local session_name = project_root:gsub("/", "_")
-
+    if project_root:find(directory) then
       local session = MiniSessions.detected[session_name]
-      if not session then
-        vim.cmd("cd " .. project_root)
-        MiniSessions.write(session_name)
+      if session then
+        -- does not auto load session if something else is shown
+        if is_something_shown() then
+          return
+        end
+
+        MiniSessions.read(session_name)
       else
-        -- Hack Minisessions.detected table setting the modify_time for the session for the current directory as the
-        -- greatest so "MiniSessions.get_latest()" retrieves the session for the current directory
-        vim.system({ "touch", MiniSessions.config.directory .. "/" .. session_name }):wait()
+        vim.fn.chdir(project_root)
+        MiniSessions.write(session_name)
       end
 
       return
     end
   end
-
-  -- If cwd is in none of the autosession_directories simply don't autoload session
-  MiniSessions.setup({ autoread = false })
 end
 
 M.setup = function()
-  local MiniSessions = require("mini.sessions")
+  require("mini.sessions").setup()
 
   vim.api.nvim_create_autocmd("VimEnter", {
     group = vim.api.nvim_create_augroup("AutoSession", { clear = true }),
     nested = true,
     once = true,
     callback = setup_autosession,
-    desc = "Create Session automatically if it doesn't exist",
+    desc = "Create and Load Session automatically",
   })
 
   vim.api.nvim_create_user_command("SSave", function(opts)
@@ -82,10 +82,6 @@ M.setup = function()
       return { "read", "write", "delete" }
     end,
     nargs = "?",
-  })
-
-  MiniSessions.setup({
-    autoread = true,
   })
 end
 
