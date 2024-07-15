7be66016118e890f3c813f4d62de31aa049cfb3d
Carlos Afonso <afonso.carlos.braga@gmail.com>
2024-07-15 20:41:58 +0100

    Make gx smarter
    Detect urls event without https://
    Detect string pattern as repository in lua config files and open its Github page
    Search word under cursor on duckduck go by default (instead of showing the default error msg)

--- nvim/.config/nvim/lua/afonsocarlos/globals.lua
+++ nvim/.config/nvim/lua/afonsocarlos/globals.lua
@@ -2,3 +2,25 @@ P = function(v)
     print(vim.inspect(v))
     return v
 end
+
+vim.ui.open = (function(overridden)
+  return function(path)
+    vim.validate({
+      path = { path, 'string' },
+    })
+    local is_uri = path:match('%w+:')
+    local is_half_url = path:match('%.com%f[%W]')
+    local is_repo = vim.bo.filetype == 'lua' and path:match('%w/%w') and vim.fn.count(path, '/') == 1
+    local is_dir = path:match('/%w')
+    if not is_uri then
+      if is_half_url then
+        path = ('https://%s'):format(path)
+      elseif is_repo then
+        path = ('https://github.com/%s'):format(path)
+      elseif not is_dir then
+        path = ('https://duckduckgo.com/?q=%s'):format(path)
+      end
+    end
+    overridden(path)
+  end
+end)(vim.ui.open)
