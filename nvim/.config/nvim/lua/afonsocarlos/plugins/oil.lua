4c866c9135786720bb0dfac6f27be0847392a465
Carlos Afonso <afonso.carlos.braga@gmail.com>
2024-09-10 10:06:35 +0100

    Add handy function to oil.nvim for finding files within current directory only

--- nvim/.config/nvim/lua/afonsocarlos/plugins/oil.lua
+++ nvim/.config/nvim/lua/afonsocarlos/plugins/oil.lua
@@ -29,6 +29,16 @@ return {
           end
         end)
       end,
+      ["<leader>ff"] = {
+        function()
+          require("telescope.builtin").find_files({
+            cwd = require("oil").get_current_dir()
+          })
+        end,
+        mode = "n",
+        nowait = true,
+        desc = "Find files in the current directory"
+      },
     },
     lsp_file_methods = {
       autosave_changes = "unmodified",
