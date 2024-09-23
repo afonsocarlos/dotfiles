b53d1ef9197d036797d0227adbae43c9c710b538
Carlos Afonso <afonso.carlos.braga@gmail.com>
2024-09-23 18:54:57 +0100

    Fully integrate mini.ai with treesitter

--- nvim/.config/nvim/lua/afonsocarlos/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/afonsocarlos/plugins/nvim-treesitter.lua
@@ -1,31 +1,4 @@
 local textobjects = {
-  select = {
-    enable = true,
-
-    -- Automatically jump forward to textobj, similar to targets.vim
-    lookahead = true,
-
-    keymaps = {
-      -- You can use the capture groups defined in textobjects.scm
-      ["af"] = { query = "@function.full", desc = "Select outer part of a method/function definition" },
-      ["if"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
-      ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
-      ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
-    },
-    -- You can choose the select mode (default is charwise 'v')
-    selection_modes = {
-      ['@parameter.outer'] = 'v', -- charwise
-      ['@function.outer'] = 'V',  -- linewise
-      ['@function.full'] = 'V',  -- linewise
-      ['@class.outer'] = 'V', -- linewise
-    },
-    -- If you set this to `true` (default is `false`) then any textobject is
-    -- extended to include preceding xor succeeding whitespace. Succeeding
-    -- whitespace has priority in order to act similarly to eg the built-in
-    -- `ap`.
-    include_surrounding_whitespace = false,
-  },
-
   swap = {
     enable = true,
     swap_next = {
