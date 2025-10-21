5ae97d9faefdcde19e855b0934bbcf5ca678621e
Carlos Afonso <afonso.carlos.braga@gmail.com>
2025-10-21 13:56:12 +0100

    install official treesitter blade parser

--- nvim/.config/nvim/lua/afonsocarlos/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/afonsocarlos/plugins/nvim-treesitter.lua
@@ -55,6 +55,7 @@ return {
       -- ensure these language parsers are installed
       ensure_installed = {
         "bash",
+        "blade",
         "c",
         "css",
         "dockerfile",
@@ -91,23 +92,6 @@ return {
           -- scope_incremental = "grc",
           -- node_decremental = "grm",
         },
-      },
-    })
-
-    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-
-    parser_config.blade = {
-      install_info = {
-        url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = {"src/parser.c"},
-        branch = "main",
-      },
-      filetype = "blade"
-    }
-
-    vim.filetype.add({
-      pattern = {
-        ['.*%.blade%.php'] = 'blade',
       },
     })
   end
