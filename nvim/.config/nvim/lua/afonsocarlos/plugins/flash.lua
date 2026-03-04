985e121883c1771a2a850a0a1dda557237bbf584
Carlos Braga <carlos.braga.partner@decathlon.com>
2026-03-04 15:05:30 +0000

    Update flash keymap trigger to backslash ("\")

--- nvim/.config/nvim/lua/afonsocarlos/plugins/flash.lua
+++ nvim/.config/nvim/lua/afonsocarlos/plugins/flash.lua
@@ -14,7 +14,7 @@ return {
   },
   -- stylua: ignore
   keys = {
-    { "?", function() require("flash").jump() end, mode = { "n", "x", "o" }, desc = "Flash" },
+    { "<Bslash>", function() require("flash").jump() end, mode = { "n", "x", "o" }, desc = "Flash" },
     { "r", function() require("flash").remote() end, mode = "o", desc = "Remote Flash" },
     { "R", function() require("flash").treesitter_search() end, mode = { "o", "x" }, desc = "Treesitter Search" },
   },
