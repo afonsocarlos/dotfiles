c6e18beb574008767e183297ddd1776f759a3539
Carlos Afonso <afonso.carlos.braga@gmail.com>
2026-08-17 10:57:55 +0100

    Add new buffer keymap for closing all buffers

--- nvim/.config/nvim/lua/afonsocarlos/keymap.lua
+++ nvim/.config/nvim/lua/afonsocarlos/keymap.lua
@@ -29,10 +29,11 @@ vim.keymap.set("n", "<leader>w", ":noa w<CR>", default_opts)
 -- Make file executable
 vim.keymap.set("n", "<leader>X", ":silent ![[ -x % ]] && chmod -x % || chmod +x %<CR>:echo 'File stat: ' . getfperm(expand('%'))<CR>", default_opts)
 -- Close buffer
+vim.keymap.set("n", "<leader>da", ":bufdo bd<CR>", default_opts)
 vim.keymap.set("n", "<leader>db", ":bdel!<CR>", default_opts)
 vim.keymap.set("n", "<leader>dd", ":Bdel<CR>", default_opts)
+vim.keymap.set("n", "<leader>de", ":Bdel #<CR>", default_opts)
 vim.keymap.set("n", "<leader>df", ":Bdel!<CR>", default_opts)
-vim.keymap.set("n", "<leader>da", ":Bdel #<CR>", default_opts)
 vim.keymap.set("n", "<leader>dt", ":tabclose<CR>", default_opts)
 -- Toggle spell check
 vim.keymap.set("n", "<F5>", ":setlocal spell! spelllang=pt_br<CR>")
